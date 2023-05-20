701fad7efc91a102c525e4afa828bf4ca904afa6
TIME <opout777@yandex.ru>
2023-05-20 18:09:49 +0300

    Need jUnit test

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,26 +2,34 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.Notification;
+import pro.sky.telegrambot.repositoryes.NotificationRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-
     private final TelegramBot telegramBot;
+    private final NotificationRepository notificationRepository;
 
-    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationRepository notificationRepository) {
         this.telegramBot = telegramBot;
+        this.notificationRepository = notificationRepository;
     }
 
     @PostConstruct
@@ -31,19 +39,74 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            if (update.message().text() != null
-                    && update.message().text().equals("/start")) {
-                if (update.message().from().languageCode().equals("ru")) {
-                    SendMessage message = new SendMessage(update.message().chat().id(),
-                            "Добро пожаловать в мой бот " + update.message().chat().firstName() + "!");
-                    SendResponse response = telegramBot.execute(message);
-                } else {SendMessage message = new SendMessage(update.message().chat().id(),
-                        "Welcome to my bot " + update.message().chat().firstName() + "!");
-                    SendResponse response = telegramBot.execute(message);}
-            }
-        });
+        updates.forEach(this::processUpdate);
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void processUpdate(Update update) {
+        try {
+            logger.info("Processing update: {}", update);
+            Message message = update.message() != null ? update.message() : update.editedMessage();
+            if (message == null) return;
+
+            String messageText = message.text();
+            if (messageText == null) return;
+
+            if ("/start".equals(messageText)) {
+                sendWelcomeMessage(message);
+            } else {
+                processNotificationMessage(message, messageText);
+            }
+        } catch (Exception e) {
+            logger.error("Error processing update: {}", update, e);
+        }
+    }
+
+    private void sendWelcomeMessage(Message message) {
+        String welcomeText = message.from().languageCode().equals("ru")
+                ? "Добро пожаловать в мой бот " + message.chat().firstName() + "!"
+                : "Welcome to my bot " + message.chat().firstName() + "!";
+        SendMessage welcomeMessage = new SendMessage(message.chat().id(), welcomeText);
+        telegramBot.execute(welcomeMessage);
+    }
+
+    private void processNotificationMessage(Message message, String messageText) {
+        Pattern pattern = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4} \\d{2}:\\d{2}) (.+)");
+        Matcher matcher = pattern.matcher(messageText);
+
+        if (matcher.matches()) {
+            saveNotification(message, matcher);
+        } else {
+            sendInvalidFormatMessage(message);
+        }
+    }
+
+    private void saveNotification(Message message, Matcher matcher) {
+        String dateTimeString = matcher.group(1);
+        String notificationText = matcher.group(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+        Notification task = new Notification(message.chat().id(), notificationText, dateTime);
+        notificationRepository.save(task);
+    }
+
+    private void sendInvalidFormatMessage(Message message) {
+        String invalidFormatText = message.from().languageCode().equals("ru")
+                ? "Некорректный формат сообщения. Используйте формат дд.мм.гггг чч:мм текст напоминания"
+                : "Incorrect message format. Use format dd.MM.yyyy HH:mm notification text";
+        SendMessage invalidFormatMessage = new SendMessage(message.chat().id(), invalidFormatText);
+        telegramBot.execute(invalidFormatMessage);
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void checker() {
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        List<Notification> notifications = notificationRepository.findByNotificationTime(now);
+        notifications.forEach(this::sendNotify);
+    }
+
+    private void sendNotify(Notification notification) {
+        SendMessage message = new SendMessage(notification.getChat_id(), notification.getNotification_text());
+        telegramBot.execute(message);
     }
 }
